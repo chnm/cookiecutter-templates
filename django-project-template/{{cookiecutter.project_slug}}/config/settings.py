753db6fdd0c9716402e6e472d3d14976433b667d
qtrinh2 <qtrinh2@gmu.edu>
2025-01-07 12:20:10 -0500

    django-debug-toolbar

--- django-project-template/{{cookiecutter.project_slug}}/config/settings.py
+++ django-project-template/{{cookiecutter.project_slug}}/config/settings.py
@@ -62,6 +62,19 @@ MIDDLEWARE = [
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
+# DEBUG
+# ------------------------------------------------------------------------------
+# django-debug-toolbar
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
